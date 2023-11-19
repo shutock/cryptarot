b985697cbddc5cb1dcbb6a14c643ebad33a0b5cd
Denis <106779859+shutock@users.noreply.github.com>
2023-11-19 08:43:55 +0300

    fix: hotfix

--- src/modules/reading/reading.component.tsx
+++ src/modules/reading/reading.component.tsx
@@ -3,11 +3,14 @@
 import { useBalance, useCards, useReading } from "@/hooks";
 import styles from "./reading.module.scss";
 import React from "react";
+import { useAccount } from "wagmi";
 
 export const Reading: React.FC = () => {
   const { data: tokens } = useBalance((state) => state);
   const { cards } = useCards((state) => state);
   const { isLoading, read, reset, data, error } = useReading((state) => state);
+
+  const { address } = useAccount();
 
   React.useEffect(() => {
     if (!tokens || !cards) return;
@@ -15,9 +18,12 @@ export const Reading: React.FC = () => {
     read({ tokens, cards });
   }, [tokens, cards, read]);
 
+  if (!address) return false;
+
   return (
     <section className={styles.section}>
       <h3 className={styles.title}>reading the spread</h3>
+      {isLoading && <div>loading...</div>}
       {data}
     </section>
   );
